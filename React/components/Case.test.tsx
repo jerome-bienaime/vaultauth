b6656d7f7934efb22e843a09d2265fc4420863fc
arriere-aride <arriere.aride@gmail.com>
2023-11-08 14:42:05 +0100

    :test_tube: test(react): add Case component test

---
+++ React/components/Case.test.tsx
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+import {
+  screen,
+  render,
+} from '@testing-library/react'
+import CaseComponent from './Case'
+import React from 'react'
+
+it('renders a Case', () => {
+  const renderer = render(
+    <CaseComponent case={3} />
+  )
+  const result = screen.getByText('3')
+  expect(result).toBeDefined()
+})
