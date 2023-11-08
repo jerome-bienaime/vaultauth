eca728a87943787d19dc48a09dc4d593f9eea744
arriere-aride <arriere.aride@gmail.com>
2023-11-08 14:55:33 +0100

    :test_tube: test(react): add Column Component test

---
+++ React/components/Column.test.tsx
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+import {
+  screen,
+  render,
+} from '@testing-library/react'
+import ColumnComponent from './Column'
+import React from 'react'
+
+it("renders a Column of Cases", () => {
+  const renderer = render(
+    <ColumnComponent columns={["a","b","c"]} />
+  )
+
+  const result = screen.getAllByText("a")
+  expect(result).toBeDefined()
+})
