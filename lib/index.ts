01c540b417b9ddb2c9b9fdb4985a6c385bfd8a16
jerome.bienaime <jerome.bienaime@gmail.com>
2023-11-08 14:08:46 +0100

    :label: chore: update library to expose types

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,9 @@
 export { default as ShuffleArraySchema } from './ShuffleArraySchema'
 export { default as ValueInputSchema } from './VaultInputSchema'
 export { default as VaultValidate } from './VaultValidate'
+export type {
+  default as ShuffleArray,
+  Column,
+} from './ShuffleArray'
+export type { default as VaultInput } from './VaultInput'
+export type { default as Case } from './Case'
