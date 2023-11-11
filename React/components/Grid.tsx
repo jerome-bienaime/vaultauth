fef90ad25762b2108aa90ae2a15fc025f6a02e9f
arriere-aride <arriere.aride@gmail.com>
2023-11-11 14:52:32 +0100

    docs(react): add Grip story

--- React/components/Grid.tsx
+++ React/components/Grid.tsx
@@ -1,21 +1,35 @@
 import React from 'react'
-import type {
-  ShuffleArray,
-  Row,
-} from '../../lib'
+import { Grid as GridUI } from 'theme-ui'
+import type { ShuffleArray, Row } from '../../lib'
 import RowComponent from './Row'
+import {
+  getTabsterAttribute,
+  Types,
+} from 'tabster'
 
 interface GridProps {
   shuffleArray: ShuffleArray
 }
 
 export default function Grid(props: GridProps) {
-  return props.shuffleArray.map(
-    (Rows: Row, index: number) => (
-      <RowComponent
-        key={index}
-        Rows={Rows}
-      />
-    )
+  return (
+    <GridUI
+      gap={2}
+      {...getTabsterAttribute({
+        groupper: {
+          tabbability:
+            Types.GroupperTabbabilities
+              .LimitedTrapFocus,
+        },
+      })}
+      tabIndex={0}
+      title='Grid'
+    >
+      {props.shuffleArray.map(
+        (rows: Row, index: number) => (
+          <RowComponent key={index} Row={rows} />
+        )
+      )}
+    </GridUI>
   )
 }
