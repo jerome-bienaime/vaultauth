b5e0cb0097580bb182a93263c2c3bbf39f8a9d3a
arriere-aride <arriere.aride@gmail.com>
2023-11-12 05:52:29 +0100

    💄chore(react): add container to Grid

--- React/components/Grid.tsx
+++ React/components/Grid.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Grid as GridUI } from 'theme-ui'
+import {
+  Container,
+  Grid as GridUI,
+} from 'theme-ui'
 import type { ShuffleArray, Row } from '../../lib'
 import RowComponent from './Row'
 import {
@@ -13,23 +16,28 @@ interface GridProps {
 
 export default function Grid(props: GridProps) {
   return (
-    <GridUI
-      gap={2}
-      {...getTabsterAttribute({
-        groupper: {
-          tabbability:
-            Types.GroupperTabbabilities
-              .LimitedTrapFocus,
-        },
-      })}
-      tabIndex={0}
-      title='Grid'
-    >
-      {props.shuffleArray.map(
-        (rows: Row, index: number) => (
-          <RowComponent key={index} Row={rows} />
-        )
-      )}
-    </GridUI>
+    <Container>
+      <GridUI
+        gap={2}
+        {...getTabsterAttribute({
+          groupper: {
+            tabbability:
+              Types.GroupperTabbabilities
+                .LimitedTrapFocus,
+          },
+        })}
+        tabIndex={0}
+        title='Grid'
+      >
+        {props.shuffleArray.map(
+          (rows: Row, index: number) => (
+            <RowComponent
+              key={index}
+              Row={rows}
+            />
+          )
+        )}
+      </GridUI>
+    </Container>
   )
 }
