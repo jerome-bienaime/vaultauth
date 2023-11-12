5cdbe48a4a517de528fca051cf03b8412a3ecd49
jerome.bienaime <jerome.bienaime@gmail.com>
2023-11-12 05:50:57 +0100

    📝 docs(react): add VaultInput story

--- React/components/VaultInput.tsx
+++ React/components/VaultInput.tsx
@@ -1,31 +1,44 @@
 import React from 'react'
 import { VaultInput } from '../../lib'
+import {
+  Box,
+  Container,
+  Grid,
+  Input,
+} from 'theme-ui'
 
 interface VaultInputProps {
   guess: VaultInput
 }
-export default function VaultInput(
+
+export default function VaultInputComponent(
   props: VaultInputProps
 ) {
   return (
-    <div className='vaultInput-container'>
-      {Array(props.guess.length)
-        .fill(0)
-        .map((_: number, index: number) => {
-          return (
-            <div
-              key={index}
-              className='vaultInput-item'
-            >
-              <input
-                type='text'
-                role="input"
-                className="vaultInput-input"
-                name={`guess-${index}`}
-              />
-            </div>
-          )
-        })}
-    </div>
+    <Container>
+      <Grid
+        className='vaultInput-container'
+        columns={4}
+        gap={1}
+      >
+        {Array(props.guess.length)
+          .fill(0)
+          .map((_: number, index: number) => {
+            return (
+              <Box
+                key={index}
+                className='vaultInput-item'
+              >
+                <Input
+                  type='text'
+                  role='input'
+                  className='vaultInput-input'
+                  name={`guess-${index}`}
+                />
+              </Box>
+            )
+          })}
+      </Grid>
+    </Container>
   )
 }
